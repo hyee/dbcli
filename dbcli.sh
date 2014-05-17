9b6ce89b56006ce5731ec445c8f68e16e8c38a89
hyee <hyeetsc@gmail.com>
2014-05-17 23:32:21 +0800

    1

--- dbcli.sh
+++ dbcli.sh
@@ -4,7 +4,7 @@ export JRE_HOME="/C/Program Files (x86)/Java/jdk7/jre/bin;"
 export TNS_ADM="/C/Oracle/product/11.2.0/client_1/network/admin"
 
 export PATH=$JRE_HOME:$PATH
-java -server -Xmx32M -cp ./lib/.:./lib/jnlua-0.9.6.jar \
+java -Xmx32M -cp ./lib/.:./lib/jnlua-0.9.6.jar \
                -Djava.library.path=./lib/ \
                -Doracle.net.tns_admin="$TNS_ADM" \
                Loader ^
