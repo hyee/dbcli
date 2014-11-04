479bedce1dfd40c2ab39cfcde9809b29984a11ea
hyee <hyeetsc@gmail.com>
2014-11-04 19:20:39 +0800

    fixed invalid low and high value for the desc command

--- oracle/desc.lua
+++ oracle/desc.lua
@@ -166,13 +166,14 @@ local desc_sql={
 				  ,'NVARCHAR2'    ,to_char(utl_raw.cast_to_nvarchar2(low_value))
 				  ,'BINARY_DOUBLE',to_char(utl_raw.cast_to_binary_double(low_value))
 				  ,'BINARY_FLOAT' ,to_char(utl_raw.cast_to_binary_float(low_value))
-				  ,'DATE',to_char(1780+to_number(substr(low_value,1,2),'XX')
-				         +to_number(substr(low_value,3,2),'XX'))||'-'
-				       ||to_number(substr(low_value,5,2),'XX')||'-'
-				       ||to_number(substr(low_value,7,2),'XX')||' '
-				       ||(to_number(substr(low_value,9,2),'XX')-1)||':'
-				       ||(to_number(substr(low_value,11,2),'XX')-1)||':'
-				       ||(to_number(substr(low_value,13,2),'XX')-1)
+				  ,'DATE',RTRIM(LTRIM(TO_CHAR(100 * (TO_NUMBER(SUBSTR(low_value, 1, 2), 'XX') - 100) +
+					                     (TO_NUMBER(SUBSTR(low_value, 3, 2), 'XX') - 100),
+					                     '0000')) || '-' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(low_value, 5, 2), 'XX'), '00')) || '-' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(low_value, 7, 2), 'XX'), '00')) || ' ' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(low_value, 9, 2), 'XX') - 1, '00')) || ':' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(low_value, 11, 2), 'XX') - 1, '00')) || ':' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(low_value, 13, 2), 'XX') - 1, '00')))
 				  ,  low_value) low_v,
                 decode(data_type
 					  ,'NUMBER'       ,to_char(utl_raw.cast_to_number(high_value))
@@ -180,13 +181,14 @@ local desc_sql={
 					  ,'NVARCHAR2'    ,to_char(utl_raw.cast_to_nvarchar2(high_value))
 					  ,'BINARY_DOUBLE',to_char(utl_raw.cast_to_binary_double(high_value))
 					  ,'BINARY_FLOAT' ,to_char(utl_raw.cast_to_binary_float(high_value))
-					  ,'DATE',to_char(1780+to_number(substr(high_value,1,2),'XX')
-					         +to_number(substr(high_value,3,2),'XX'))||'-'
-					       ||to_number(substr(high_value,5,2),'XX')||'-'
-					       ||to_number(substr(high_value,7,2),'XX')||' '
-					       ||(to_number(substr(high_value,9,2),'XX')-1)||':'
-					       ||(to_number(substr(high_value,11,2),'XX')-1)||':'
-					       ||(to_number(substr(high_value,13,2),'XX')-1)
+					  ,'DATE', RTRIM(LTRIM(TO_CHAR(100 * (TO_NUMBER(SUBSTR(high_value, 1, 2), 'XX') - 100) +
+					                     (TO_NUMBER(SUBSTR(high_value, 3, 2), 'XX') - 100),
+					                     '0000')) || '-' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(high_value, 5, 2), 'XX'), '00')) || '-' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(high_value, 7, 2), 'XX'), '00')) || ' ' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(high_value, 9, 2), 'XX') - 1, '00')) || ':' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(high_value, 11, 2), 'XX') - 1, '00')) || ':' ||
+					       LTRIM(TO_CHAR(TO_NUMBER(SUBSTR(high_value, 13, 2), 'XX') - 1, '00')))
 					  ,  high_value) hi_v
 		FROM   all_tab_cols
 		WHERE  upper(owner)=:1  and table_name=:2
