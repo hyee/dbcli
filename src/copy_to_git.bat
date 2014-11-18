6db1381687a8db38e47c47898fa83182b743f4ef
hyee <hyeetsc@gmail.com>
2014-11-18 23:11:51 +0800

    moved mnk files to the src folder, it is used to build the chm file

--- src/copy_to_git.bat
+++ src/copy_to_git.bat
@@ -7,10 +7,12 @@ del /s "%target%\*.chm"
 del /s "%target%\*.bat"
 del /s "%target%\*.txt"
 del /s "%target%\*.bak"
+del /s "%target%\*.mnk"
 xcopy  . "%target%" /S /Y  /exclude:excludes.txt
 
 COPY /Y "C:\Software\eclipse\workspace\dbcli\src\Loader.java" .\src\
 COPY /Y .\src\Loader.java "%target%\src"
 COPY /Y .\copy_to_git.bat "%target%\src"
 COPY /Y .\excludes.txt "%target%\src"
+COPY /Y .\docs\*.mnk "%target%\src"
 pause
