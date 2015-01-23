75a9f925bb233e2e5b5bc840ec698e8ed1275385
hyee <hyeetsc@gmail.com>
2015-01-24 01:54:29 +0800

    .

--- oracle/ora/deptree.sql
+++ oracle/ora/deptree.sql
@@ -1,24 +1,41 @@
-/*[[Show object dependency, usage: ora deptree [-c] [-f] [owner.]name 
+/*[[
+    Show object dependency, usage: ora deptree [-c|-p] [-t|-f] [owner.]name 
+    Options: 
+       p: Show target object's depending objects(default)
+       c: Show the objects that depend on target object
+       t: Show dependence tree(Default)
+       f: Show object list, instead of tree view
+       
     --[[
-       &F2  : P={d_obj#},C={p_obj#}
+       
        &F1  : P={p_obj#},C={d_obj#}
+       &F2  : P={d_obj#},C={p_obj#}
+       &F3  : P={rcon#},C={con#}
+       &F4  : P={con#},C={rcon#}
        &CC  : T={1},F={0}
        &SRT : T={'9'},F={1}
        &DST : T={},f={DISTINCT}
     --]]
 ]]*/
 SET FEED OFF
+SET PRINTSIZE 10000
 WITH obj AS
- (SELECT --+materialize no_expand
-         MAX(object_id) KEEP(DENSE_RANK FIRST ORDER BY DECODE(owner,'PUBLIC','ZZZZ',owner)) o,
-         max(owner) KEEP(DENSE_RANK FIRST ORDER BY DECODE(owner,'PUBLIC','ZZZZ',owner)) owner
-  FROM   ALL_OBJECTS us
-  WHERE  owner in (decode(instr(:V1,'.'),0,USER,upper(regexp_substr(:V1,'^[^\.]+'))),decode(instr(:V1,'.'),0,'PUBLIC','#'))
-  AND    object_name = upper(decode(instr(:V1,'.'),0,:V1,regexp_substr(:V1,'[^\.]+$')))
-  AND    subobject_name IS NULL),
+ (SELECT /*+materialize*/ * 
+  FROM (
+      SELECT --+no_expand
+             object_id o,
+             owner
+      FROM   ALL_OBJECTS
+      WHERE  owner in (decode(instr(:V1,'.'),0,USER,upper(regexp_substr(:V1,'^[^\.]+'))),decode(instr(:V1,'.'),0,'PUBLIC','#'))
+      AND    object_name = upper(decode(instr(:V1,'.'),0,:V1,regexp_substr(:V1,'[^\.]+$')))
+      AND    subobject_name IS NULL
+      ORDER BY DECODE(OWNER,'PUBLIC','ZZZZZZ',OWNER),OBJECT_ID DESC)
+  WHERE ROWNUM<2),
 res AS(
 SELECT --+ordered use_nl(dep op us) no_merge(dep) no_expand
-       LPAD(' ',(lv-1)*4*&CC) ||us.name||'.'||op.name OBJECT_NAME,
+       LPAD(' ',lv*2*&CC) ||us.name||'.'||op.name
+          ||nvl2(con#,(select '('||name||')' from sys.con$ c where c.con#=dep.con#),'') OBJECT_NAME,
+          op.obj# OBJECT_ID,
           decode(op.type#,
               0,'NEXT OBJECT',
               1,'INDEX',
@@ -81,13 +98,25 @@ SELECT --+ordered use_nl(dep op us) no_merge(dep) no_expand
               95,'CUBE BUILD PROCESS',
               'UNDEFINED')  object_type,
               decode(op.status, 0, 'N/A', 1, 'VALID', 'INVALID') STATUS,
-              op.ctime,
-              op.mtime
-FROM   (select dep.*,obj.*,level lv
-        from obj,sys.dependency$ dep 
-        CONNECT BY nocycle dep.&F2 = PRIOR dep.&F1 START WITH dep.&F2 =obj.o
-        order siblings by dep.&F1) dep, sys.obj$ op,sys.user$ us
+              op.ctime CREATED,
+              op.mtime LAST_DDL
+FROM   (
+        SELECT o d_obj#,o p_obj#,0 lv,null con#,owner from obj WHERE &CC=1
+        UNION ALL
+        SELECT * FROM(
+            SELECT d_obj#,p_obj#,level lv,null con#,owner
+            FROM   obj,sys.dependency$ dep 
+            CONNECT BY nocycle dep.&F2 = PRIOR dep.&F1 
+            START WITH dep.&F2 =obj.o
+            ORDER siblings by dep.&F1)
+        UNION ALL
+        SELECT obj#,robj#,level lv, &F3,owner
+        FROM obj,sys.cdef$ a 
+        START WITH obj#=obj.o and type#!=7
+        CONNECT BY NOCYCLE &F4 = PRIOR &F3
+        ) dep, 
+        sys.obj$ op,sys.user$ us
 WHERE  dep.&F1 = op.obj#
 AND    op.owner#=us.user#
-AND    (us.name!='SYS' and op.type#!=5 or dep.owner in('SYS','PUBLIC')))
-select &DST * from res WHERE object_type NOT IN('UNDEFINED') ORDER BY &SRT;
+AND    ((us.name!='SYS' or dep.owner in('SYS','PUBLIC')) and (op.type#!=5 or us.name in('SYS','PUBLIC'))))
+select lpad(rownum,5)||' | ' "#", a.* FROM (select &DST * from res WHERE object_type NOT IN('UNDEFINED') ORDER BY &SRT) A;
