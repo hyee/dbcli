fcdfdd1f7747bd1d3c77aba1e88c4299bffe5bc3
hyee <hyeetsc@gmail.com>
2016-04-23 19:01:22 +0800

    enhancement, new features and fixes related to graph chart

--- lua/graph.lua
+++ lua/graph.lua
@@ -3,55 +3,59 @@ local json,math,graph,cfg=env.json,env.math,env.class(env.scripter),env.set
 local template,cr
 --[[
     Please refer to "http://dygraphs.com/options.html" for the graph options that used in .chart files
+    The content of .chart file must follow the lua table syntax. for example: {a=..,b={...}}
     Settings:
         set ChartSeries <number>: Define the max series to be shown in the chart.
                                   If the data contains many series, only show the top <number> deviations
-    Common options from dygraphs:
-        ylabel,title,height,rollPeriod,drawPoints,logscale,fillGraph,stackedGraph,stepPlot,strokePattern
+    Common options from dygraphs(available values are all true/false):
+        ylabel,title,height,rollPeriod,drawPoints,logscale,fillGraph,stackedGraph,
+        stepPlot,strokePattern,plotter
     Other options:
         _attrs="<sql_statement>" : Select statement to proceduce the attributes, 
                                    field name matches the attribute name,
-                                   and must return only one row, it can also can be used as a variable inside "_sql" option
+                                   and must return only one row, the output fields can also can be used as a variable inside "_sql" option
+                                   i.e.:In below case, the chart title is defined as 'THIS IS TITLE', and &title, &group by variables can be used. 
+                                       _attrs="select 'THIS IS TITLE' title, 'event_name' group_by from dual"
         _sql  ="<sql_statement>" : The select statement to produce the graph data
-                                   1st field : the value of X-Asis, mainly be a time value
+                                   X-label: the value of the 1st field, mainly be a time value
                                    _pivot=false:
-                                       Asis Name  = 2nd+ field names
-                                       Asis Value = 2nd+ field values(number)
-                                       2nd+ fields: field name as the curve name and value as the Y-Asis
-                                       Example:
+                                        Asis Name  = 2nd+ field names
+                                        Asis Value = 2nd+ field values(number)
+                                        2nd+ fields: field name as the curve name and value as the Y-Asis
+                                        Example:
                                            date(x-label)  latch1  latch2
                                            --------------------------------
                                            2015-1-1        99      98
                                            2015-1-1        97      96
                                    _pivot=true:
-                                       Asis Name  = 2nd field value(string)
-                                       Asis Value = 3rd+ values(number), if colums(N)>3, then create N-2 charts 
-                                       Example:
+                                        Axis Name  = 2nd field value(string)
+                                        Axis Value = 3rd+ values(number), if colums(N)>3, then create N-2 charts 
+                                        Example:
                                            date(x-label)  name   Y1  Y2 
                                            -----------------------------
                                            2015-1-1      latch1  99  24 
                                            2015-1-1      latch2  98  23 
-                                                                     ||(convert into)
-                                                                     \/
+                                                                    ||(split into)
+                                                                    \/
                                            date(Y1) latch1  latch2      date(Y2)  latch1  latch2
-                                           ------------------------- +  ------------------------
+                                           ------------------------- + ------------------------
                                            2015-1-1  99      98         2015-1-1   24      23  
                                            
                                    _pivot="mixed":
-                                       Asis Name  = <2nd field value(string)> + <3rd+ field name>
-                                       Asis Value = 3rd+ values
-                                       Example:
+                                        Axis Name  = <2nd field value(string)> + <3rd+ field name>
+                                        Axis Value = 3rd+ values
+                                        Example:
                                            date(x-label)  name   Y1  Y2
                                            ----------------------------
                                            2015-1-1      latch1  99  24
                                            2015-1-1      latch2  98  23 
                                                      ||(convert into)
                                                      \/
-                                           date(x-label)  latch1[Y1] latch1[Y2] latch2[Y1] latch2[Y1]
+                                           date(x-label)  latch1[Y1] latch1[Y2] latch2[Y1] latch2[Y2]
                                            ----------------------------------------------------------
                                            2015-1-1        99         24        98          23
-                                            
-        _pivot=true|false|"mixed": indicate if pivot the >2nd numberic fields, refer to above  
+                                        Refer to "racping.chart" for more example        
+        _pivot=true|false|"mixed": indicate if pivot the >2nd numberic fields, refer to above
         _ylabels={"<label1>",...}: Customize the ylabel for each chart, not not define then use the names from "_sql"
         _range="<Time range>"    : Used in sub-title, if not specified then auto-caculate the range
         multiScale=true|false    : False for org value, and true to display the data based on deviation%(value*100/average)
@@ -60,6 +64,21 @@ local template,cr
 function graph:ctor()
     self.command='graph'
     self.ext_name='chart'
+    cfg.init("ChartSeries",12,set_param,"core","Number of top series to be show in graph chart(see command 'chart')",'1-20')
+end
+
+function graph:rehash(script_dir,ext_name,extend_dirs)
+    template=nil
+    return env.scripter.rehash(self,script_dir,ext_name,extend_dirs)
+end
+
+
+function graph:run_sql(sql,args,cmd,file)
+    if type(sql)=="table" then
+        for i=1,#sql do self:run_sql(sql[i],args[i],cmd[i],file[i]) end
+        return
+    end
+
     if not template then
         template=env.load_data(env.WORK_DIR.."lib"..env.PATH_DEL.."dygraphs.html",false)
         env.checkerr(type(template)=="string",'Cannot load file "dygraphs.html" in folder "lib"!')
@@ -77,20 +96,12 @@ function graph:ctor()
         <hr/><br/><br/>]]
         --template=template:gsub('@GRAPH_FIELDS',cr)
     end
-    cfg.init("ChartSeries",12,set_param,"core","Number of top series to be show in graph chart(see command 'chart')",'1-500')
-end
 
-
-function graph:run_sql(sql,args,cmd,file)
-    if type(sql)=="table" then
-        for i=1,#sql do self:run_sql(sql[i],args[i],cmd[i],file[i]) end
-        return
-    end
-    local units,rs,rows={}
+    local charts,rs,rows={}
     
     local context,err=loadstring(('return '..sql):gsub(self.comment,"",1))
     env.checkerr(context,"Error when loading file %s: %s",file,err)
-   
+
     context=context()
     env.checkerr(type(context)=="table" and type(context._sql)=="string","Invalid definition, should be a table with '_sql' property!")
     local default_attrs={
@@ -135,7 +146,7 @@ function graph:run_sql(sql,args,cmd,file)
     end
 
     rs=self.db:exec(sql,args)
-    local title,txt,keys,values,collist,temp=string.char(0x1),{},{},{},{},{}
+    local title,csv,xlabels,values,collist,temp=string.char(0x1),{},{},{},{},{}
     
     local function getnum(val)
         if not val then return 0 end
@@ -145,7 +156,10 @@ function graph:run_sql(sql,args,cmd,file)
     local counter,range_begin,range_end=-1
     rows=self.db.resultset:rows(rs,-1)
     local head=table.remove(rows,1)
+    local maxaxis=cfg.get('ChartSeries')
     table.sort(rows,function(a,b) return a[1]<b[1] end)
+    --print(table.dump(rows))
+
     if pivot=="mixed" then
         local data={}
         for k,v in ipairs(rows) do
@@ -159,10 +173,12 @@ function graph:run_sql(sql,args,cmd,file)
         table.insert(rows,1,head)
     end
     
+    local cols=#rows[1]
     while true do
         counter=counter+1
         local row=rows[counter+1]
         if not row then break end
+        for i=1,cols do if row[i]==nil then row[i]=0 end end
         if counter>0 and row[1]~="" then
             local x=row[1]
             if not range_begin then
@@ -176,15 +192,15 @@ function graph:run_sql(sql,args,cmd,file)
             range_begin,range_end=range_begin>x and x or range_begin, range_end<x and x or range_end
         end
         --For pivot, col1=label, col2=Pivot,col3=y-value
-        --collist: {label={1:nth-label,2:count,3:1st-min,4:1st-max,5+:sum(value)-of-each-chart},...}
+        --collist: {label={1:nth-axis,2:count,3:1st-min,4:1st-max,5+:sum(value)-of-each-chart},...}
         if pivot then
             local x,label,y=row[1],row[2],{table.unpack(row,3)}
             for i=#y,1,-1 do
                if rows[1][i+2]=="RNK_" or rows[1][i+2]=="RND_" then table.remove(y,i) end
             end
-            if #keys==0 then
-                units,keys[1],values[title]=y,title,{x}
-                env.checkerr(#units>0,'Pivot mode should have at least 3 columns!')
+            if #xlabels==0 then
+                charts,xlabels[1],values[title]=y,title,{x}
+                env.checkerr(#charts>0,'Pivot mode should have at least 3 columns!')
                 print('Fetching data into HTML file...')
             else
                 if not collist[label] then
@@ -194,33 +210,34 @@ function graph:run_sql(sql,args,cmd,file)
                 local col=collist[label]
                 if not values[x] then
                     values[x]={x,table.unpack(temp)}
-                    keys[#keys+1]=x
+                    xlabels[#xlabels+1]=x
                 end
                 values[x][col[1]]=y
                 local val=getnum(y[1])
-                if counter>0 and val then
+                if counter>0 then
                     col[2],col.data[#col.data+1]=col[2]+1,val
                     col[3],col[4]=math.min(val,col[3] or val),math.max(val,col[4] or val)
                 end
                 for i=1,#y do
-                    col[4+i]=(col[4+i] or 0)+(getnum(y[i]) or 0)
+                    col[4+i]=(col[4+i] or 0)+getnum(y[i])
                 end 
             end
         else
+            csv={}
             if not values[title] then values[title]=row end
-            for i=2,#row do
-                if #txt==0 then
+            for i=2,math.min(cols,maxaxis+1) do
+                if #csv==0 then
                     collist[row[i]]={i,0,data={}}
+                    --row[i]='"'..row[i]:gsub('"','""')..'"' --quote title fields
                 else
                     local col,val=collist[values[title][i]],getnum(row[i])
-                    if counter>0 and val then
+                    if counter>0 then
                         col[2],col[5],col.data[#col.data+1]=col[2]+1,(col[5] or 0)+val,val
                         col[3],col[4]=math.min(val,col[3] or val),math.max(val,col[4] or val)
                     end
-                    if not row[i] then row[i]=0 end
                 end
             end
-            txt[#txt+1]=table.concat(row,',')
+            csv[#csv+1]=table.concat(row,',',1,math.min(cols,maxaxis+1))
         end
     end
 
@@ -260,30 +277,31 @@ function graph:run_sql(sql,args,cmd,file)
     output:add_calc_ratio(2)
     output:sort(#output.data[1],true)
     output:print(true)
-    local data,legends,temp=output.data,{},{}
+    local data,axises,temp=output.data,{},{}
     table.remove(data,1)
-    for i=math.max(1,#data-cfg.get('ChartSeries')+1),#data do
-        legends[#legends+1],temp[#temp+1]=data[i][1],0
+    for i=#data,math.max(1,#data-maxaxis+1),-1 do
+        axises[#axises+1],temp[#temp+1]=data[i][1],0
     end
     --Generate graph data
     self.dataindex,self.data=0,{}
     
     if pivot then
         --Sort the columns by sum(value)
-        for idx=1,#units do
-            local txt,avgs={},{}
-            for k,v in ipairs(keys) do
-                local row={values[v][1],table.unpack(temp)}
-                for i=1,#legends do
-                    local col=collist[legends[i]]
-                    local cell=values[v][col[1]]
-                    avgs[i],avgs[legends[i]]=legends[i],col[4+idx]/col[2]
-                    row[i+1]=k==1 and cell or cell==0 and cell or cell and cell[idx] or 0
+        for idx=1,#charts do
+            local csv,avgs={},{}
+            for rownum,xlabel in ipairs(xlabels) do
+                local row={values[xlabel][1],table.unpack(temp)}
+                for i=1,#axises do
+                    local col=collist[axises[i]]
+                    local cell=values[xlabel][col[1]]
+                    avgs[i],avgs[axises[i]]=axises[i],col[4+idx]/col[2]
+                    row[i+1]=type(cell)=="table" and cell[idx] or cell or 0
+                    --if rownum==1 then row[i+1]='"'..row[i+1]:gsub('"','""')..'"' end --The titles
                 end
-                txt[k]=table.concat(row,',')
+                csv[rownum]=table.concat(row,',')
             end
             self.dataindex=self.dataindex+1
-            self.data[self.dataindex]={table.concat(txt,'\n'),avgs}
+            self.data[self.dataindex]={table.concat(csv,'\n'),avgs}
         end
     else
         local avgs={}
@@ -291,7 +309,7 @@ function graph:run_sql(sql,args,cmd,file)
             avgs[#avgs+1],avgs[k]=k,v[5]/v[2]
         end
         self.dataindex=self.dataindex+1
-        self.data[self.dataindex]={table.concat(txt,'\n'),avgs}
+        self.data[self.dataindex]={table.concat(csv,'\n'),avgs}
     end
 
     local replaces={
@@ -308,7 +326,7 @@ function graph:run_sql(sql,args,cmd,file)
     default_attrs.title=nil
     for i=1,self.dataindex do
         replaces['@GRAPH_INDEX']=i
-        default_attrs.ylabel=ylabels[i] or default_ylabel or units[i]
+        default_attrs.ylabel=ylabels[i] or default_ylabel or charts[i]
         default_attrs._avgs=self.data[i][2]
         if default_attrs.ylabel then
             default_attrs.title="Unit: "..default_attrs.ylabel
