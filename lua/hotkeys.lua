77973c201f1f60c4bc398e67c0f581be42509d0c
hyee <hyeetsc@gmail.com>
2017-07-06 20:15:29 +0800

    improving jline3

--- lua/hotkeys.lua
+++ lua/hotkeys.lua
@@ -1,21 +1,53 @@
 local env=env
 local hotkeys={}
-function hotkeys.help()
-
-    return [[
-        Ctrl+G                       : Cancle command
-        Ctrl+D                       : Delete char
-        Alt+BackSpace                : Kill previous word
-        Alt+Left                     : Move to previous word
-        Alt+Right                    : Move to next word
-        Shift+Left                   : Scroll left
-        Shift+Right                  : Scroll right
-        Ctrl+C  or 'Q'               : Abort running command
-        Ctrl+L                      * : Clear screen
-        Ctrl+U                       : Kill all words that before cursor
-        Ctrl+K                       : Kill all words that after cursor]]
-end
-
+local map={
+    ["\8"]="BACKSPACE",
+    ["^[^H"]="ALT-BACKSPACE",
+    ["\9"]="TAB",
+    ["^[[2~"]="INSERT",
+    ["^[[3~"]="DEL",
+    ["^[[5~"]="PGUP",
+    ["^[[6~"]="PGON",
+    ["^[[H"]="HOME",
+    ["^[[1;%dH"]="HOME",
+    ["^[[4~"]="END",
+    ["^[[1;%dF"]="END",
+    ["^[OP"]="F1",
+    ["^[[1;%dP"]="F1",
+    ["^[OQ"]="F2",
+    ["^[[1;%dQ"]="F1",
+    ["^[OR"]="F3",
+    ["^[[1;%dR"]="F1",
+    ["^[OS"]="F4",
+    ["^[[1;%dS"]="F1",
+    ["^[[15~"]="F5",
+    ["^[[15;%d~"]="F5",
+    ["^[[17~"]="F6",
+    ["^[[17;%d~"]="F6",
+    ["^[[18~"]="F7",
+    ["^[[18;%d~"]="F7",
+    ["^[[19~"]="F8",
+    ["^[[19;%d~"]="F8",
+    ["^[[20~"]="F9",
+    ["^[[20;%d~"]="F9",
+    ["^[[21~"]="F10",
+    ["^[[21;%d~"]="F10",
+    ["^[[23~"]="F11",
+    ["^[[23;%d~"]="F11",
+    ["^[[24~"]="F12",
+    ["^[[24;%d~"]="F12",
+    ["^[[D"]="LEFT",
+    ["^[OD"]="LEFT",
+    ["^[[1;%dD"]="LEFT",
+    ["^[[C"]="RIGHT",
+    ["^[OC"]="RIGHT",
+    ["^[[1;%dC"]="RIGHT",
+    ["^[[A"]="UP",
+    ["^[OA"]="UP",
+    ["^[[1;%dA"]="UP",
+    ["^[[B"]="DOWN",
+    ["^[[1;%dB"]="DOWN",
+    ["^[OB"]="DOWN"}
 function hotkeys.call(event,_,x)
     local maps=console:getKeyMap("-L");
     local keys,keys1={},{}
@@ -33,15 +65,38 @@ function hotkeys.call(event,_,x)
         return
     end
     local hdl=env.grid.new()
-    hdl:add{"Key","*","Description",'|',"Key","*","Description"}
+    hdl:add{"Key","*","Description",'Code','|',"Key","*","Description",'Code'}
     local row
     for _,keys in ipairs(keys1) do
-        local key,desc=table.unpack(keys)
-        key=key:gsub('"(.-)"',function(s) return ' $HEADCOLOR$'..(console:translateKey(s))..'$NOR$ ' end)
+        local key,desc,found=table.unpack(keys)
+        local code=key:gsub('"(.-)"',' %1 ')
+        key=key:gsub('"(.-)"',function(s)
+            local s1=s
+            for k,v in pairs(map) do
+                k=k:gsub("([%^%[%]])","%%%1"):gsub("%%d","(%1)")
+                local p
+                if k:find('%d') then
+                    local idx=tonumber(s:match(k))
+                    if idx then
+                        idx=idx-1
+                        local p=" + "
+                        if bit.band(idx,4)>0 then p=p.."CTRL-" end
+                        if bit.band(idx,2)>0 then p=p.."ALT-" end
+                        if bit.band(idx,1)>0 then p=p.."SHIFT-" end
+                        p=p..v.." + "
+                        s=s:gsub(k,p)
+                    end
+                else
+                    s=s:gsub(k,v)
+                end
+            end
+            s=s:gsub("%^%[%^(.)","+ CTRL-ALT-%1"):gsub('%^%[(.)',' + ALT-%1'):gsub('%^(.)',' + CTRL-%1'):gsub('^%s*%+ ',''):gsub(' %+%s*$','')
+            return ' $HEADCOLOR$'..s..'$NOR$ ' 
+        end)
         if not row then 
-            row={key,' ',desc} 
+            row={key,' ',desc,code} 
         else
-            row[#row+1],row[#row+2],row[#row+3],row[#row+4]='|',key,' ',desc
+            row[#row+1],row[#row+2],row[#row+3],row[#row+4],row[#row+5]='|',key,' ',desc,code
             hdl:add(row)
             row=nil
         end
@@ -53,7 +108,7 @@ end
 
 
 function hotkeys.onload()
-    env.set_command(nil,"KEYMAP",{"Show available hot keys. type '@@NAME' for more information.",hotkeys.help},hotkeys.call,false,2)
+    env.set_command(nil,"KEYMAP",{"Show available hot keys. type '@@NAME' for more information."},hotkeys.call,false,2)
 end
 
 return hotkeys
---
+++ src/java/org/dbcli/ConEmuOutputStream.java
@@ -0,0 +1,76 @@
+package org.dbcli;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.win32.W32APIOptions;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class ConEmuOutputStream extends OutputStream {
+    static {
+        NativeLibrary nativeLibrary = NativeLibrary.getInstance("ConEmuHk" + (System.getProperty("os.arch").equals("x86") ? "" : "64"), W32APIOptions.UNICODE_OPTIONS);
+        Native.register(ConEmuOutputStream.class, nativeLibrary);
+    }
+
+    static final native boolean WriteProcessed(String in_lpBuffer, int in_nLength, IntByReference out_lpNumberOfCharsWritten);
+
+    static final native boolean WriteProcessed3(String in_lpBuffer, int in_nLength, IntByReference out_lpNumberOfCharsWritten, Pointer in_hConsoleOutput);
+
+    private final Pointer console;
+    private ByteBuffer buff = ByteBuffer.allocateDirect(8196);
+    private IntByReference charsWritten = new IntByReference();
+
+    public ConEmuOutputStream() {
+        this(Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE));
+    }
+
+    public ConEmuOutputStream(Pointer console) {
+        super();
+        this.console = console;
+        this.buff.order(ByteOrder.nativeOrder());
+    }
+
+    @Override
+    public final synchronized void write(final int b) throws IOException {
+        if (buff.position() >= buff.limit()) flush();
+        buff.put((byte) b);
+    }
+
+    @Override
+    public final synchronized void write(final byte b[], final int off, final int len) throws IOException {
+        if (buff.remaining() <= len) flush();
+        if (buff.remaining() >= len) buff.put(b, off, len);
+        else if (off == 0 && len == b.length) writeStdOut(new String(b));
+        else {
+            byte[] b1 = new byte[len];
+            System.arraycopy(b, off, b1, 0, len);
+            writeStdOut(new String(b1));
+        }
+    }
+
+    @Override
+    public final void write(final byte b[]) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    @Override
+    public final synchronized void flush() throws IOException {
+        final int len = buff.position();
+        if (len > 0) {
+            buff.flip();
+            byte[] b = new byte[len];
+            buff.get(b);
+            buff.clear();
+            writeStdOut(new String(b));
+        }
+    }
+
+    private final void writeStdOut(final String str) {
+        WriteProcessed3(str, str.length(), charsWritten, console);
+    }
+}
