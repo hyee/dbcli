74cd6d994fde879e83fc74f1cba76b6d53f968d1
hyee <hyeetsc@gmail.com>
2018-11-19 19:32:28 +0800

    update

--- oracle/ora/ash/ashchain.sql
+++ oracle/ora/ash/ashchain.sql
@@ -9,12 +9,13 @@ This script references Tanel Poder's script
         &range : default={sample_time BETWEEN NVL(TO_DATE(NVL(:V2,:STARTTIME),'YYMMDDHH24MI'),SYSDATE-7) AND NVL(TO_DATE(NVL(:V3,:ENDTIME),'YYMMDDHH24MI'),SYSDATE)}, snap={sample_time>=sysdate - nvl(:V1,60)/86400}, f1={}
         &snap:   default={--} snap={}
         &pname : default={decode(session_type,'BACKGROUND',program2)} p={program2}
-        &group : default={curr_obj#}, p={p123}
+        &group : default={curr_obj#}, p={p123}, phase={phase}
         &grp1  : default={sql_ids}, sid={sids}
         &grp2  : default={sql_id}, sid={sid}, none={sample_id}
         &unit  : default={1}, dash={10}
         &INST1 : default={inst_id}, dash={instance_number}
         &OBJ   : default={dba_objects}, dash={(select obj# object_id,object_name from dba_hist_seg_stat_obj)}
+        @tmodel: 11.2={time_model} default={to_number(null)}
         @CHECK_ACCESS_OBJ  : dba_objects={&obj}, default={all_objects}
         @INST: 11.2={'@'|| a.BLOCKING_INST_ID}, default={'@'||a.&inst1}
         @secs: 11.2={round(sum(least(delta_time,nvl(tm_delta_db_time,delta_time)))*1e-6,2) db_time,} default={&unit,}
@@ -32,7 +33,7 @@ var chose varchar2
 var filter2 number;
 
 declare
-    target varchar2(2000) := q'[select a.*,&INST1 inst,SESSION_ID||'@'||&INST1 SID,nullif(a.blocking_session|| &INST,'@') b_sid,sample_time+0 stime from ]'||:V8||' a where '||:range;
+    target varchar2(2000) := q'[select a.*,&tmodel tmodel,&INST1 inst,SESSION_ID||'@'||&INST1 SID,nullif(a.blocking_session|| &INST,'@') b_sid,sample_time+0 stime from ]'||:V8||' a where '||:range;
     chose varchar2(2000) := '1';
 BEGIN
     :filter2 := 1;
@@ -64,29 +65,49 @@ BEGIN
                             '('||REGEXP_REPLACE(REGEXP_SUBSTR(a.program, '[^@]+'), '\d', 'n')||')'
                         END || ' ' program2,
                         NVL2(a.event,a.event||CASE WHEN p3text='class#'
-                            THEN ' ['||(SELECT class FROM bclass WHERE r = a.p3)||']' ELSE null END,'ON CPU')|| ' ' event2,
+                                THEN ' ['||(SELECT nvl(max(class),'class# '||a.p3) FROM bclass WHERE r = a.p3)||']' ELSE null END,
+                            'ON CPU' ||CASE WHEN p3text='class#'
+                                THEN ' ['||(SELECT nvl(max(class),'class# '||a.p3) FROM bclass WHERE r = a.p3)||']'
+                                ELSE nvl2(a.p1text,' ['||trim(p1text||' '||p2text||' '||p3text)||']','') END)
+                        || ' ' event2,
                         replace(nvl2(p1text,p1text||' #'||case when p1>power(2,32) then to_char(p1,'0XXXXXXXXXXXXXXX') else ''||p1 end,'')
                             ||nvl2(p2text,'/'||p2text||' #'||case when p2>power(2,32) then to_char(p2,'0XXXXXXXXXXXXXXX') else ''||p2 end,'')
                             ||nvl2(p3text,'/'||p3text||' #'
                                 || case when p3>power(2,32) then to_char(p3,'0XXXXXXXXXXXXXXX') 
                                         when p3text='class#' then (SELECT class FROM bclass WHERE r = a.p3) 
-                                        else ''||p3 end,''),'# #',' #') p123
+                                        else ''||p3 end,''),'# #',' #') p123,
+                        trim(decode(bitand(tmodel,power(2, 3)),0,'','in_connection_mgmt ') || 
+                            decode(bitand(tmodel,power(2, 4)),0,'','in_parse ') || 
+                            decode(bitand(tmodel,power(2, 7)),0,'','in_hard_parse ') || 
+                            decode(bitand(tmodel,power(2,10)),0,'','in_sql_execution ') || 
+                            decode(bitand(tmodel,power(2,11)),0,'','in_plsql_execution ') || 
+                            decode(bitand(tmodel,power(2,12)),0,'','in_plsql_rpc ') || 
+                            decode(bitand(tmodel,power(2,13)),0,'','in_plsql_compilation ') || 
+                            decode(bitand(tmodel,power(2,14)),0,'','in_java_execution ') || 
+                            decode(bitand(tmodel,power(2,15)),0,'','in_bind ') || 
+                            decode(bitand(tmodel,power(2,16)),0,'','in_cursor_close ') || 
+                            decode(bitand(tmodel,power(2,17)),0,'','in_sequence_load ') || 
+                            decode(bitand(tmodel,power(2,18)),0,'','in_inmemory_query ') || 
+                            decode(bitand(tmodel,power(2,19)),0,'','in_inmemory_populate ') || 
+                            decode(bitand(tmodel,power(2,20)),0,'','in_inmemory_prepopulate ') || 
+                            decode(bitand(tmodel,power(2,21)),0,'','in_inmemory_repopulate ') || 
+                            decode(bitand(tmodel,power(2,22)),0,'','in_inmemory_trepopulate ') || 
+                            decode(bitand(tmodel,power(2,23)),0,'','in_tablespace_encryption ')) phase
               FROM   ash_base a
               LEFT   JOIN (SELECT DISTINCT b_sid, stime, 0 chose 
-                           FROM ash_base WHERE b_sid IS NOT NULL
-                           ) b
+                           FROM ash_base WHERE b_sid IS NOT NULL) b
               ON     (a.sid = b.b_sid AND a.stime = b.stime)
-              LEFT JOIN dba_users  u 
+              LEFT JOIN dba_users  u
               ON    (a.user_id = u.user_id)
               WHERE  b.chose IS NOT NULL
               OR     a.b_sid IS NOT NULL),
             chains AS (
-                SELECT /*+NO_EXPAND*/
+                SELECT /*+NO_EXPAND PARALLEL(4)*/
                       level lvl,
                       sid w_sid,
                       SYS_CONNECT_BY_PATH(case when :filter2 = 1 then 1 when &filter then 1 else 0 end ,',') is_found,
-                      REPLACE(trim('>' from regexp_replace(SYS_CONNECT_BY_PATH(nvl(sql_id,program2), '>')||decode(connect_by_isleaf,1,nvl2(b_sid,'>(Idle)','')),'(>.+?)\1+','\1 +')), '>', ' > ') sql_ids,
-                      REPLACE(trim('>' from regexp_replace(SYS_CONNECT_BY_PATH(&pname ||event2, '>')||decode(connect_by_isleaf,1,nvl2(b_sid,'>(Idle)','')),'(>.+?)\1+','\1 +')), '>', ' > ') path, -- there's a reason why I'm doing this (ORA-30004 :)
+                      REPLACE(trim('>' from regexp_replace(SYS_CONNECT_BY_PATH(nvl(sql_id,program2), '>')||decode(connect_by_isleaf,1,nvl2(b_sid,'>(Idle)','')),'(>.+?)\1+','\1 +',2)), '>', ' > ') sql_ids,
+                      REPLACE(trim('>' from regexp_replace(SYS_CONNECT_BY_PATH(&pname ||event2, '>')||decode(connect_by_isleaf,1,nvl2(b_sid,'>(Idle)','')),'(>.+?)\1+','\1 +',2)), '>', ' > ') path, -- there's a reason why I'm doing this (ORA-30004 :)
                       REPLACE(trim('>' from regexp_replace(SYS_CONNECT_BY_PATH(sid,'>')||decode(connect_by_isleaf,1,nullif('>'||b_sid,'>')),'(>.+?)\1+','\1 +')), '>', ' > ') sids,
                       &exec_id sql_exec,
                       CONNECT_BY_ROOT &group root_&group,
@@ -112,7 +133,7 @@ BEGIN
         OPEN :cur FOR
             WITH bclass AS (SELECT class, ROWNUM r from v$waitstat),
             ash_base as &target,
-            ash_data AS (SELECT /*+ordered swap_join_inputs(b) use_hash(a b u) SWAP_JOIN_INPUTS(u)  no_expand*/
+            ash_data AS (SELECT /*+ordered swap_join_inputs(b) swap_join_inputs(c) swap_join_inputs(u)  use_hash(a b u c)  no_expand*/
                         a.*, 
                         nvl2(b.chose,0,1) is_root,
                         u.username,
@@ -122,47 +143,61 @@ BEGIN
                         ELSE
                             '('||REGEXP_REPLACE(REGEXP_SUBSTR(a.program, '[^@]+'), '\d', 'n')||')'
                         END || ' ' program2,
-                        NVL2(a.event,a.event||CASE WHEN p3text='class#'
-                                          THEN ' ['||(SELECT class FROM bclass WHERE r = a.p3)||']' ELSE null END,'ON CPU')
-                        || ' ' event2,
+                        NVL(a.event,'ON CPU') ||
+                        CASE WHEN c.class IS NOT NULL THEN ' ['||c.class||']'
+                             WHEN a.event IS NULL THEN nvl2(a.p1text,' ['||trim(p1text||' '||p2text||' '||p3text)||']','')
+                        END || ' ' event2,
                         replace(nvl2(p1text,p1text||' #'||case when p1>power(2,32) then to_char(p1,'0XXXXXXXXXXXXXXX') else ''||p1 end,'')
                             ||nvl2(p2text,'/'||p2text||' #'||case when p2>power(2,32) then to_char(p2,'0XXXXXXXXXXXXXXX') else ''||p2 end,'')
                             ||nvl2(p3text,'/'||p3text||' #'
                                 || case when p3>power(2,32) then to_char(p3,'0XXXXXXXXXXXXXXX') 
-                                        when p3text='class#' then (SELECT class FROM bclass WHERE r = a.p3) 
-                                        else ''||p3 end,''),'# #',' #') p123
+                                        when c.class is not null then c.class
+                                        else ''||p3 end,''),'# #',' #') p123,
+                        trim(decode(bitand(tmodel,power(2, 3)),0,'','in_connection_mgmt ') || 
+                            decode(bitand(tmodel,power(2, 4)),0,'','in_parse ') || 
+                            decode(bitand(tmodel,power(2, 7)),0,'','in_hard_parse ') || 
+                            decode(bitand(tmodel,power(2,10)),0,'','in_sql_execution ') || 
+                            decode(bitand(tmodel,power(2,11)),0,'','in_plsql_execution ') || 
+                            decode(bitand(tmodel,power(2,12)),0,'','in_plsql_rpc ') || 
+                            decode(bitand(tmodel,power(2,13)),0,'','in_plsql_compilation ') || 
+                            decode(bitand(tmodel,power(2,14)),0,'','in_java_execution ') || 
+                            decode(bitand(tmodel,power(2,15)),0,'','in_bind ') || 
+                            decode(bitand(tmodel,power(2,16)),0,'','in_cursor_close ') || 
+                            decode(bitand(tmodel,power(2,17)),0,'','in_sequence_load ') || 
+                            decode(bitand(tmodel,power(2,18)),0,'','in_inmemory_query ') || 
+                            decode(bitand(tmodel,power(2,19)),0,'','in_inmemory_populate ') || 
+                            decode(bitand(tmodel,power(2,20)),0,'','in_inmemory_prepopulate ') || 
+                            decode(bitand(tmodel,power(2,21)),0,'','in_inmemory_repopulate ') || 
+                            decode(bitand(tmodel,power(2,22)),0,'','in_inmemory_trepopulate ') || 
+                            decode(bitand(tmodel,power(2,23)),0,'','in_tablespace_encryption ')) phase
               FROM   ash_base a
               LEFT   JOIN (SELECT DISTINCT b_sid, stime, 0 chose 
                            FROM ash_base WHERE b_sid IS NOT NULL) b
               ON     (a.sid = b.b_sid AND a.stime = b.stime)
               LEFT JOIN dba_users  u 
               ON    (a.user_id = u.user_id)
+              LEFT JOIN bclass c
+              ON    (a.p3text='class#' and a.p3=c.r)
               WHERE  b.chose IS NOT NULL
               OR     a.b_sid IS NOT NULL),
             chains AS (
-                SELECT greatest(length(sql_ids)-length(replace(sql_ids,'>')),length(p)-length(replace(p,'>'))) lvl,
-                       case when isleaf=1 and b_sid is not null then
-                           case when b_sid like '%@'||inst then ' > (Idle)' else ' > (Remote)' end
-                       end idle,
-                       a.* 
-                FROM (
-                    SELECT /*+NO_EXPAND PARALLEL(4)*/
-                           sid w_sid,b_sid,
-                           rownum-level r,
-                           inst,
-                           case when :filter2 = 1 then 1 when &filter then 1 else 0 end is_found,
-                           TRIM('>' FROM regexp_replace(SYS_CONNECT_BY_PATH(trim(decode(:grp2,'sample_id','x','sql_id',nvl(sql_id, program2),&grp2)),'>'),'(>.+?)\1+','\1(+)',2)) sql_ids,
-                           TRIM('>' FROM regexp_replace(SYS_CONNECT_BY_PATH(trim(&pname || event2), '>'), '(>.+?)\1+', '\1(+)',2)) p, 
-                           &exec_id sql_exec,
-                           connect_by_isleaf isleaf,
-                           CONNECT_BY_ROOT decode(:grp2,'sample_id',trim(&pname || event2),'sql_id',nvl(sql_id, program2),&grp2) root_sql,
-                           trim(decode(:grp2,'sample_id',' ','sql_id',nvl(sql_id, program2),&grp2))  sq_id,
-                           trim(&pname || event2) env,
-                           &group,
-                           &io io
-                    FROM  ash_data d
-                    CONNECT BY NOCYCLE (PRIOR d.b_sid = d.sid AND PRIOR stime = stime)
-                    START WITH is_root=1) a),
+                SELECT /*+NO_EXPAND PARALLEL(4)*/
+                       sid w_sid,b_sid,
+                       rownum-level r,
+                       inst,
+                       case when :filter2 = 1 then 1 when &filter then 1 else 0 end is_found,
+                       TRIM('>' FROM regexp_replace(SYS_CONNECT_BY_PATH(trim(decode(:grp2,'sample_id','x','sql_id',nvl(sql_id, program2),&grp2)),'>'),'(>.+?)\1+','\1(+)',2)) sql_ids,
+                       TRIM('>' FROM regexp_replace(SYS_CONNECT_BY_PATH(trim(&pname || event2), '>'), '(>.+?)\1+', '\1(+)',2)) p, 
+                       &exec_id sql_exec,
+                       connect_by_isleaf isleaf,
+                       CONNECT_BY_ROOT decode(:grp2,'sample_id',trim(&pname || event2),'sql_id',nvl(sql_id, program2),&grp2) root_sql,
+                       trim(decode(:grp2,'sample_id',' ','sql_id',nvl(sql_id, program2),&grp2))  sq_id,
+                       trim(&pname || event2) env,
+                       &group,
+                       &io io
+                FROM  ash_data d
+                CONNECT BY NOCYCLE (PRIOR d.b_sid = d.sid AND PRIOR stime = stime)
+                START WITH is_root=1),
             calc AS (
                SELECT /*+materialize*/ * 
                FROM (SELECT a.*,
@@ -171,13 +206,13 @@ BEGIN
                  FROM (
                   SELECT /*+leading(b a)*/
                          sum(count(1)) over(partition by root_sql) rnk,
-                         max(&group||' ('||grp_count||')') keep(dense_Rank last order by grp_count) &group,
+                         max(nvl2(&group,&group||' ('||grp_count||')','')) keep(dense_Rank last order by grp_count) &group,
                          root_sql,
                          sql_ids,
                          trim(p)||idle p,
                          SUM(&UNIT*isleaf) delta,
-                         max(sq_id) sq_id,
-                         max(env||idle) env,
+                         max(nullif(sq_id||plus1,'(+)')) sq_id,
+                         max(env||plus2||replace(idle,'>',' > ')) env,
                          COUNT(DISTINCT sql_exec) execs,
                          SUM(&UNIT) aas,
                          sum(io) io,
@@ -186,7 +221,13 @@ BEGIN
                       SELECT A.*,
                              b.is_found,
                              b.rnk,
-                             sum(&UNIT) over(partition by sql_ids,p,&group) grp_count
+                             greatest(length(sql_ids)-length(replace(sql_ids,'>')),length(p)-length(replace(p,'>'))) lvl,
+                             case when isleaf=1 and b_sid is not null then
+                                 case when b_sid like '%@'||inst then '>(Unknown)' else '>(Remote)' end
+                             end idle,
+                             case when sql_ids like '%(+)' then '(+)' end plus1,
+                             case when p like '%(+)' then '(+)' end plus2,
+                             sum(&UNIT) over(partition by sql_ids,p,&group,case when isleaf=1 and b_sid is not null then 1 else 0 end) grp_count
                       FROM   chains a,
                              (SELECT b.*, dense_rank() OVER(ORDER BY leaves DESC,paths) rnk
                               FROM   (SELECT r, MAX(sql_ids) || MAX(p) paths,max(is_found) is_found,COUNT(1) OVER(PARTITION BY MAX(sql_ids) || MAX(p)) leaves 
