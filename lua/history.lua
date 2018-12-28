5b58e6a0b087ab7fa651dc12d92f528da4c8e1e2
hyee <hyeetsc@gmail.com>
2018-12-28 09:47:04 +0800

    fixes

--- lua/history.lua
+++ lua/history.lua
@@ -29,7 +29,7 @@ end
 
 local is_changed,current_line,current_index
 
-local cmds={HIS=1,HISTORY=1,["/"]=1,R=1,EDIT=1,ED=1,L=1,LIST=1,CHANGE=1,C=1}
+local cmds={HIS=1,HISTORY=1,["/"]=1,R=1,EDIT=1,ED=1,L=1,LIST=1,CHANGE=1,C=1,HELP=1,SET=1,KEYMAP=1}
 
 function history:capture(cmd,args,res,is_internal,command_text,clock)
     if #env.RUNNING_THREADS>1 or not args then return end
@@ -76,7 +76,7 @@ end;
 
 function history.rerun()
     local file=load_file()
-    if file then env.eval_line(lastcommand.text,true,true) end
+    if file then env.eval_line(file,true,true) end
 end
 
 function history.edit_buffer(file)
@@ -97,34 +97,56 @@ function history.edit_buffer(file)
     end 
 end
 
-local fmt='%s%-3s| %s'
+local fmt='%s%-3s$PROMPTCOLOR$|$NOR$ %s'
 function history.show_command(m,n)
     local file=load_file()
     if not file or file=='' then return end
-    local target=tostring(m):lower()
+   
     local lines=file:split('\r?\n')
-    if target=='last' or target=='*' or not current_index then 
-        current_index=#lines
-    elseif tonumber(target) then
-        current_index=tonumber(target)
+    local function get_index(target)
+        local idx
+        if target=='last' then
+            idx=#lines
+        elseif target=='*' then
+            idx=current_index or #lines 
+        elseif tonumber(target) then
+            idx=tonumber(target)
+            if idx<0 then
+                idx=#lines+1+idx
+            end
+        end
+        return idx
     end
 
-    local line=lines[current_index]
-    env.checkerr(line,'String not found[#'..current_index..'].')
+    local m1=get_index(m) or current_index or #lines
+    local n1=get_index(n) or m1
+    local line=lines[m1]
+
+    env.checkerr(line and lines[n1],'Lines not found[#'..m1..'-'..n1..'].')
+    if m and (m~='*' and n~='*') or not current_index then current_index=n1 end;
+
+    if m and m1 == n1 then
+        return print(fmt:format('*',''..m1,line))
+    end
+
+    local output={}
+    local b,e=1,#lines
 
     if m then
-        return print(fmt:format('*',''..current_index,line))
+        b,e=math.min(m1,n1),math.max(m1,n1)
     end
 
     for k,v in ipairs(lines) do
-        lines[k]=fmt:format(k==current_index and '*' or ' ',k,v)
+        if k>=b and k<=e then
+            output[#output+1]=fmt:format(k==current_index and '*' or ' ',k,v)
+        end
     end
-    
-    print(table.concat(lines,'\n'))
+    print(table.concat(output,'\n'))
 end
 
 function history.change_command(m)
     env.checkerr(m,'Nothing to change.')
+    if m:sub(1,1)=='/' then m=m:sub(2) end
     local file=load_file()
     if not file then return end
     local o,n=table.unpack(m:split('/',true))
@@ -151,15 +173,13 @@ function history.set_editor(name,editor)
     return editor
 end
 
-
-
 function history.onload()
     cfg.init("HISSIZE",50,history.set_editor,"core","Max size of historical commands",'0 - 999')
     cfg.init({"EDITOR",'_EDITOR'},env.IS_WINDOWS and 'notepad' or 'vi',history.set_editor,"core","The editor to edit the buffer")
     event.snoop("AFTER_SUCCESS_COMMAND",history.capture,history)
     env.set_command(history,{'history','his'},"Show/run historical commands. Usage: @@NAME [index|last]",history.show,false,2)
     env.set_command(history,{'r','/'},"Rerun the previous command.",history.rerun,false,2)
-    env.set_command(nil,{'l'},"Show the previous command.",history.show_command,false,3)
+    env.set_command(nil,{'l'},"Show the previous command. Usage: @@NAME [last|*|<num>]",history.show_command,false,3)
     env.set_command(nil,{'c'},"Change command. Usage: @@NAME [<text>|<old>/<new>] ",history.change_command,false,2)
     env.set_command({history,{'EDIT','ED'},"Use the program that defined in 'set editor' to edit the buffer in order to run with '/'.",history.edit_buffer,false,2,is_blocknewline=true})
 end
