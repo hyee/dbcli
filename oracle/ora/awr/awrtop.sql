620ba29f952c546e21cab3bfb9fa05746b14fff7
hyee <hyeetsc@gmail.com>
2019-02-18 08:20:34 +0800

    updates'

--- oracle/ora/awr/awrtop.sql
+++ oracle/ora/awr/awrtop.sql
@@ -2,30 +2,38 @@
     --[[
         &filter: s={1=1},u={PARSING_SCHEMA_NAME=nvl('&0',sys_context('userenv','current_schema'))},f={}
         &BASE : s={sql_id}, m={signature}
+        &v2   : df={ela} default={}
     --]]
 ]]*/
 set feed off
-col value,avg format smhd2
+col &v2,avg format smhd2
 ORA _sqlstat
+col pct for pct2
 
 WITH qry as (SELECT coalesce(upper(:V1),''||:instance,'A') inst,
                     lower(nvl(:V2,'ela')) typ,
                     to_timestamp(coalesce(:V3,:starttime,to_char(sysdate-7,'YYMMDDHH24MI')),'YYMMDDHH24MI') st,
                     to_timestamp(coalesce(:V4,:endtime,to_char(sysdate,'YYMMDDHH24MI')),'YYMMDDHH24MI')  ed from dual)
 SELECT /*+ordered use_nl(a b)*/
-     r "#",
      a.sql_id,
-     plan_hash,
+     phvs,
+     plan_hash top_phv,
      execs,
      parse,
-     val value,
+     val &v2,
+     pct,
      val/nvl(execs,nullif(parse,0)) "AVG",
-     substr(regexp_replace(to_char(SUBSTR(sql_text, 1, 500)),'['||chr(10)||chr(13)||chr(9)||' ]+',' '),1,200) text
-FROM (SELECT rownum r,a.* from(
+     (SELECT trim(substr(regexp_replace(to_char(SUBSTR(sql_text, 1, 500)),'['||chr(10)||chr(13)||chr(9)||' ]+',' '),1,200)) text FROM DBA_HIST_SQLTEXT WHERE SQL_ID=regexp_substr(a.sql_id,'\w+') and dbid=a.dbid and rownum<2) SQL_TEXT
+FROM (SELECT rownum r,
+             ratio_to_report(val) over() pct,
+             a.* 
+      from(
           SELECT /*+ordered use_nl(s hs)*/
                    max(sql_id)  KEEP(dense_rank LAST ORDER BY elapsed_time_total)||case when '&base'!='sql_id' and regexp_like(&base,'^\d+$') then '|'||&base end sql_id,
                    max(sql_id) sqlid,
+                   max(dbid) dbid,
                    qry.typ,
+                   count(distinct s.plan_hash_value) phvs,
                    ''||MAX(s.plan_hash_value) KEEP(dense_rank LAST ORDER BY elapsed_time_total) plan_hash,
                    decode(typ,'mem',MAX(s.sharable_mem)/1024/1024,
                       SUM(decode(nvl(qry.typ,'ela'),
@@ -41,7 +49,7 @@ FROM (SELECT rownum r,a.* from(
                                     'fetch',END_OF_FETCH_COUNT,
                                     'row',ROWS_PROCESSED,
                                     'px',PX_SERVERS_EXECS,
-                                    s.elapsed_time)))val,
+                                    s.elapsed_time))) val,
                    nullif(SUM(s.executions),0) execs,
                    sum(s.PARSE_CALLS) parse
             FROM   qry,&&awr$sqlstat s
@@ -49,9 +57,6 @@ FROM (SELECT rownum r,a.* from(
             AND    s.end_time BETWEEN qry.st and qry.ed
             AND    (&filter)
             GROUP  BY &base,qry.typ
-            ORDER  BY decode(qry.typ,'exec',execs,'parse',parse,val) desc nulls last)a) a,
-           Dba_Hist_Sqltext b
-WHERE  a.sqlid = b.sql_id(+)
-AND    r<=50
-
-order  by 1
+            ORDER  BY decode(qry.typ,'exec',execs,'parse',parse,val) desc nulls last) a) a
+WHERE  r<=50
+order  by r
--- oracle/ora/awr/awrtop2.sql
+++ oracle/ora/awr/awrtop2.sql
@@ -1,8 +1,9 @@
 /*[[
-    Show AWR Top SQLs for a specific period. Usage: @@NAME {[0|<inst>] [a|<sql_id>] [total|avg] [yymmddhhmi] [yymmddhhmi] [exec|ela|cpu|io|cc|fetch|rows|load|parse|read|write|mem]} [-m] 
+    Show AWR Top SQLs for a specific period. Usage: @@NAME {[0|<inst>] [a|<sql_id>] [total|avg] [yymmddhhmi] [yymmddhhmi] [exec|ela|cpu|io|cc|buff|fetch|rows|load|parse|read|write|mem|offload|cell]} [-m] 
     --[[
         &grp: s={sql_id}, m={signature}
         &filter: s={1=1},u={PARSING_SCHEMA_NAME=nvl('&0',sys_context('userenv','current_schema'))},f={}
+        &v6: df={ela}
         @ver: 11.2={} default={--}
     --]]
 ]]*/
@@ -10,7 +11,7 @@
 ORA _sqlstat
 
 col ela for smhd2
-col iowait,cpu,clwait,apwait,plsql,ccwait format pct1
+col iowait,cpu,clwait,apwait,plsql,ccwait,&v6# format pct1
 col reads,writes,mem,cellio,oflin,oflout,buff format kmg
 col execs,FETCHES,loads,parses,rows,PX format tmb
 
@@ -21,7 +22,8 @@ WITH qry as (SELECT nvl(upper(NVL(:V1,:INSTANCE)),'A') inst,
                     to_timestamp(coalesce(:V5,:endtime,to_char(sysdate,'YYMMDDHH24MI')),'YYMMDDHH24MI') ed,
                     lower(nvl(:V6,'ela')) sorttype
              FROM Dual)
-SELECT &grp,
+SELECT pct &v6#,
+       &grp,
        plan_hash,
        last_call,
        execs,
@@ -33,72 +35,75 @@ SELECT &grp,
        reads,writes,buff,
        RWS "ROWS",
        PX,
-       (SELECT substr(regexp_replace(to_char(SUBSTR(sql_text, 1, 500)),'['||chr(10)||chr(13)||chr(9)||' ]+',' '),1,100) text FROM DBA_HIST_SQLTEXT WHERE SQL_ID=a.sq_id and dbid=a.dbid and rownum<2) SQL_TEXT
-FROM   (SELECT &grp,sq_id,dbid,
-               plan_hash,
-               to_char(lastest,'MM-DD"|"HH24:MI') last_call,
-               execs,
-               LOADs,
-               parses,
-               seens,
-               mem / exe1 mem,
-               ela / exe1 ela,
-               CPU / ela CPU,
-               iowait / ela iowait,
-               ccwait / ela ccwait,
-               clwait / ela clwait,
-               apwait / ela apwait,
-               PLSQL / ela PLSQL,
-               READ / exe1 READS,
-               buff / exe1 buff,
-               WRITE / exe1 WRITES,
-               FETCH / exe1 FETCHES,
-               RWS / exe1 rws,
-               PX / exe1 PX,
-               oflin/exe1 oflin,
-               oflout/exe1 oflout,
-               cellio/exe1 cellio,
-               row_number() over(order by decode(sorttype, 'exec', execs, 'load', loads, 'parse', parses,
-                                               'mem', mem, 'ela', ela, 'cpu', cpu, 'io', iowait, 'plsql', plsql,
-                                               'read', read, 'write', write, 'fetch', fetch,'buff',buff,
-                                               'rows', rws, 'px', px,'cc',ccwait,'offload',oflin,'cell',cellio)/exe1 desc nulls last) r
-        FROM   (SELECT --+no_expand
-                       &grp,
-                       max(dbid) dbid,
-                       max(sql_id) sq_id,
-                       plan_hash_value plan_hash,
-                       qry.sorttype,
-                       count(1) SEENS,
-                       MAX(begin_interval_time) lastest,
-                       SUM(executions) execs,
-                       SUM(LOADS) LOADs,
-                       SUM(PARSE_CALLS) parses,
-                       AVG(sharable_mem/1024/ 1024) mem,
-                       SUM(elapsed_time ) ela,
-                       SUM(cpu_time ) CPU,
-                       SUM(iowait) iowait,
-                       SUM(CCWAIT) ccwait,
-                       SUM(CLWAIT) clwait,
-                       SUM(apwait) apwait,
-                       SUM(PLSEXEC_TIME+JAVEXEC_TIME) PLSQL,
-                       SUM(cellio) cellio,
-                       SUM(oflin) oflin,
-                       SUM(oflout) oflout,
-                       SUM(greatest(disk_reads,s.phyread)) READ,
-                       SUM(nvl(phywrite,direct_writes)) WRITE,
-                       sum(buffer_gets) buff,
-                       SUM(FETCHES) FETCH,
-                       SUM(ROWS_PROCESSED) RWS,
-                       SUM(PX_SERVERS_EXECS) PX,
-                       decode(max(qry.calctype),
-                              'avg',
-                              SUM(NVL(NULLIF(executions, 0),
-                                      NULLIF(PARSE_CALLS, 0))),
-                              1) exe1
-                FROM   qry,&&awr$sqlstat s
-                WHERE  (qry.sqid = &grp or qry.sqid is null)
-                AND    (&filter)
-                AND    s.begin_interval_time between qry.st and ed
-                AND    (qry.inst in('A','0') or qry.inst= ''||s.instance_number)
-                GROUP  BY &grp, plan_hash_value,qry.sorttype)) a
+       (SELECT trim(substr(regexp_replace(to_char(SUBSTR(sql_text, 1, 500)),'['||chr(10)||chr(13)||chr(9)||' ]+',' '),1,100)) text FROM DBA_HIST_SQLTEXT WHERE SQL_ID=a.sq_id and dbid=a.dbid and rownum<2) SQL_TEXT
+FROM   (SELECT a.*, row_number() over(order by val desc nulls last) r,
+               ratio_to_report(val) over() pct
+        FROM (
+            SELECT &grp,sq_id,dbid,
+                   plan_hash,
+                   to_char(lastest,'MM-DD"|"HH24:MI') last_call,
+                   execs,
+                   LOADs,
+                   parses,
+                   seens,
+                   mem / exe1 mem,
+                   ela / exe1 ela,
+                   CPU / ela CPU,
+                   iowait / ela iowait,
+                   ccwait / ela ccwait,
+                   clwait / ela clwait,
+                   apwait / ela apwait,
+                   PLSQL / ela PLSQL,
+                   READ / exe1 READS,
+                   buff / exe1 buff,
+                   WRITE / exe1 WRITES,
+                   FETCH / exe1 FETCHES,
+                   RWS / exe1 rws,
+                   PX / exe1 PX,
+                   oflin/exe1 oflin,
+                   oflout/exe1 oflout,
+                   cellio/exe1 cellio,
+                   decode(sorttype, 'exec', execs, 'load', loads, 'parse', parses,
+                                                   'mem', mem, 'ela', ela, 'cpu', cpu, 'io', iowait, 'plsql', plsql,
+                                                   'read', read, 'write', write, 'fetch', fetch,'buff',buff,
+                                                   'rows', rws, 'px', px,'cc',ccwait,'offload',oflin,'cell',cellio)/exe1 val
+            FROM   (SELECT --+no_expand
+                           &grp,
+                           max(dbid) dbid,
+                           max(sql_id) sq_id,
+                           plan_hash_value plan_hash,
+                           qry.sorttype,
+                           count(1) SEENS,
+                           MAX(begin_interval_time) lastest,
+                           SUM(executions) execs,
+                           SUM(LOADS) LOADs,
+                           SUM(PARSE_CALLS) parses,
+                           AVG(sharable_mem/1024/ 1024) mem,
+                           SUM(elapsed_time ) ela,
+                           SUM(cpu_time ) CPU,
+                           SUM(iowait) iowait,
+                           SUM(CCWAIT) ccwait,
+                           SUM(CLWAIT) clwait,
+                           SUM(apwait) apwait,
+                           SUM(PLSEXEC_TIME+JAVEXEC_TIME) PLSQL,
+                           SUM(cellio) cellio,
+                           SUM(oflin) oflin,
+                           SUM(oflout) oflout,
+                           SUM(greatest(disk_reads,s.phyread)) READ,
+                           SUM(nvl(phywrite,direct_writes)) WRITE,
+                           sum(buffer_gets) buff,
+                           SUM(FETCHES) FETCH,
+                           SUM(ROWS_PROCESSED) RWS,
+                           SUM(PX_SERVERS_EXECS) PX,
+                           decode(max(qry.calctype),
+                                  'avg',
+                                  SUM(NVL(NULLIF(executions, 0),
+                                          NULLIF(PARSE_CALLS, 0))),
+                                  1) exe1
+                    FROM   qry,&&awr$sqlstat s
+                    WHERE  (qry.sqid = &grp or qry.sqid is null)
+                    AND    (&filter)
+                    AND    s.begin_interval_time between qry.st and ed
+                    AND    (qry.inst in('A','0') or qry.inst= ''||s.instance_number)
+                    GROUP  BY &grp, plan_hash_value,qry.sorttype)) a)a
 WHERE  r <= 50
