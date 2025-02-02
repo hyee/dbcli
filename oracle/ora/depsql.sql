11191e00940a41ea7e3a8db2133126b5f30cd676
hyee <hyeetsc@gmail.com>
2025-02-02 21:56:29 +0800

    change of `snap system`

--- oracle/ora/depsql.sql
+++ oracle/ora/depsql.sql
@@ -12,11 +12,14 @@ set printsize 50
 SELECT /*+ OPT_PARAM('_fix_control' '26552730:0') opt_param('optimizer_dynamic_sampling' 0)*/
      sql_id,
      COUNT(DISTINCT plan_hash) plans,
+     TRIM(',' FROM decode(bitand(SUM(DISTINCT FLAG),1),1,'SQLAREA,')
+                 ||decode(bitand(SUM(DISTINCT FLAG),2),2,'SQLMON,')
+                 ||decode(bitand(SUM(DISTINCT FLAG),4),4,'AWR')) source,
      SUM(ela) ela,
      SUM(ela) / nullif(SUM(execs),0) avg_ela,
      SUM(EXECUTING) EXECUTING,
      MAX(sql_text) sql_text
-FROM   (
+FROM  (
     SELECT *
     FROM TABLE(GV$(CURSOR( --
           SELECT /*+no_merge(a) leading(a) use_nl(b) opt_estimate(table b rows=1000000)*/
@@ -25,12 +28,12 @@ FROM   (
                 elapsed_time ela,
                 greatest(executions, 1) execs,
                 USERS_EXECUTING EXECUTING,
-                TRIM(regexp_replace(substr(b.sql_text, 1, 250), '\s+', ' ')) sql_text
-          FROM  v$object_dependency a, v$sql b
+                TRIM(regexp_replace(substr(b.sql_text, 1, 250), '\s+', ' ')) sql_text,
+                1 flag
+          FROM  (SELECT DISTINCT from_hash,from_address FROM v$object_dependency WHERE to_name = :object_name AND to_owner = :object_owner) a, 
+                 v$sql b
           WHERE a.from_hash = b.hash_value
           AND   a.from_address = b.address
-          AND   a.to_name = :object_name
-          AND   a.to_owner = :object_owner &ver
           UNION ALL
           SELECT /*+no_merge(a) leading(a) use_nl(b) opt_estimate(table b rows=1000000)*/
                 b.sql_id,
@@ -38,7 +41,8 @@ FROM   (
                 GREATEST(ELAPSED_TIME,CPU_TIME+APPLICATION_WAIT_TIME+CONCURRENCY_WAIT_TIME+CLUSTER_WAIT_TIME+USER_IO_WAIT_TIME+QUEUING_TIME) ela,
                 CASE WHEN PX_SERVER# IS NULL AND sql_text IS NOT NULL THEN 1 ELSE 0 END execs,
                 0 EXECUTING,
-                TRIM(regexp_replace(substr(b.sql_text, 1, 250), '\s+', ' ')) sql_text
+                TRIM(regexp_replace(substr(b.sql_text, 1, 250), '\s+', ' ')) sql_text,
+                2 flag
           FROM  (SELECT /*+cardinality(1)*/ 
                        DISTINCT key 
                  FROM  v$sql_plan_monitor 
@@ -54,7 +58,8 @@ FROM   (
            elapsed_time_delta,
            executions_delta,
            0,
-           TRIM(regexp_replace(to_char(substr(b.sql_text, 1, 250)), '\s+', ' ')) sql_text
+           TRIM(regexp_replace(to_char(substr(b.sql_text, 1, 250)), '\s+', ' ')) sql_text,
+           4 flag
     FROM   (SELECT /*+cardinality(1)*/
                    DISTINCT sql_id, dbid
             FROM   dba_hist_sql_plan
