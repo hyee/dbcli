b52e44f8c333e4d725ad8f3ac9904c6ae6007ad5
hyee <hyeetsc@gmail.com>
2026-01-25 16:36:33 +0800

    new script 'show rollseg', misc fixes on 'load'

---
+++ oracle/show/rollseg.sql
@@ -0,0 +1,70 @@
+/*[[Show Rollback Segment Usages]]*/
+col "USED|BYTES,SEG|BYTES,FILE|BYTES,MAX|BYTES,INCREMENT|BYTES,Undo|Size" for kmg2
+col "SEG|BLOCKS,FILE|BLOCKS,MAX|BLOCKS,SEG|EXTENTS,Undo|ROWS,Undo|BLOCKS" for TMB2
+col "Used|Rate" for pct2
+
+PRO TOP 50 SEGMENTS BY SEGMENT BYTES:
+PRO =================================
+SELECT * 
+FROM (
+    SELECT /*+OUTLINE_LEAF USE_HASH(s f e)
+            opt_param('_optimizer_sortmerge_join_enabled','false') 
+            opt_param('optimizer_index_cost_adj' 1000)
+            table_stats(SYS.X$KTFBFE SAMPLE BLOCKS=512)
+            table_stats(SYS.X$KTFBUE SAMPLE BLOCKS=512) 
+            table_stats(SYS.SEG$ SAMPLE BLOCKS=1024)*/
+            segment_name "SEGMENT|NAME",
+            tablespace_name "TABLESPACE|NAME",
+            file_id "FILE|ID",
+            relative_fno "RELATIVE|FILE#",
+            s.status "SEG|STATUS",
+            e.bytes "SEG|BYTES",
+            e.blocks "SEG|BLOCKS",
+            e.extents "SEG|EXTENTS",
+            '|' "|",
+            t.inst_id "Txn|Inst",
+            t.xacts "Txn|Count",
+            t.rssize "Undo|Size",
+            t.used_urec "Undo|ROWS",
+            t.used_ublk "Undo|BLOCKS",
+            '||' "||",
+            f.bytes "FILE|BYTES",
+            e.file_bytes "USED|BYTES",
+            round(e.file_bytes/f.bytes,4) "Used|Rate",
+            f.autoextensible "AUTO|EXTEND",
+            decode(f.autoextensible, 'YES', f.maxbytes, f.bytes) "MAX|BYTES",
+            f.blocks "FILE|BLOCKS",
+            decode(f.autoextensible, 'YES', f.maxblocks, f.blocks) "MAX|BLOCKS",
+            decode(f.autoextensible, 'YES', f.increment_by, 0) "INCREMENT|BYTES"
+    FROM   dba_rollback_segs s
+    JOIN   dba_data_files f
+    USING  (relative_fno, file_id, tablespace_name)
+    LEFT   JOIN (
+        SELECT /*+no_merge*/
+                segment_name,
+                tablespace_name,
+                file_id,
+                relative_fno,
+                SUM(bytes) bytes,
+                SUM(SUM(bytes)) over (partition by tablespace_name, file_id, relative_fno) file_bytes,
+                SUM(blocks) blocks,
+                COUNT(file_id) extents
+        FROM   dba_extents
+        WHERE  segment_type LIKE '%UNDO%'
+        GROUP  BY segment_name, tablespace_name, file_id, relative_fno) e
+    USING  (segment_name, tablespace_name, file_id, relative_fno)
+    LEFT JOIN TABLE(GV$(CURSOR(
+        SELECT userenv('instance') inst_id,
+               greatest(count(t.xidusn),max(r.xacts)) xacts,
+               u.name segment_name,
+               max(r.rssize) rssize,
+               sum(t.used_urec) used_urec,
+               sum(t.used_ublk) used_ublk 
+        FROM   v$rollstat r
+        JOIN   v$rollname u ON r.usn = u.usn
+        LEFT JOIN   v$transaction t ON t.xidusn = r.usn
+        GROUP  BY u.name
+    ))) t USING(segment_name)
+    ORDER BY "SEG|BYTES" DESC)
+WHERE rownum<=50;
+
